4776033a7016ee555029d3914412aa7ef111e163
Alwaleed  Alharbi <waleedbdulaziz@gmail.com>
2025-05-04 11:04:22 +0300

    lab

---
+++ main.js
@@ -0,0 +1,43 @@
+// First Exercise
+let rectLength = 10,
+    rectWidth = 20;
+
+const total = rectLength * rectWidth;
+console.log(`The area of the rectangle is: ${total}`);
+
+
+// Second Exercise
+let tempCelsius = 25;
+
+let tempFahrenheit = (tempCelsius * 9 / 5) + 32;
+
+console.log(`${tempCelsius}°C is ${tempFahrenheit}°F`);
+
+let tempFahrenheit2 = 77;
+
+let tempCelsius2 = (tempFahrenheit2 - 32) * 5 / 9;
+
+console.log(`${tempFahrenheit2}°F is ${tempCelsius2}°C`);
+
+
+
+// Third Exercise 
+let hour = 6;
+
+let seconds = hour * 3600;
+
+console.log(`${hour} hours = ${seconds} seconds`);
+
+
+// Fourth Exercise
+let first = "Alwaleed";
+let second = "Alharbi";
+
+console.log(first + " " + second);
+
+
+var bill = 10.25 + 3.99 + 7.15;
+var tip = bill * 0.15;
+var total = bill + tip;
+
+console.log(total);
